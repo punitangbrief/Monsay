6ddce75aee91ed270903efcde1c6542b766f89fe
ralphskiee <ralphskieee@yahoo.com>
2017-10-08 09:47:55 +0800

    Eto na po

---
+++ application/views/templates/footer.php
@@ -0,0 +1,102 @@
+<!--footer-->
+<div class="divider">
+<hr />
+</div>
+
+<div class="row	fill">
+<div class="col-lg-4 col-md-4 ">
+<ul class="footText">
+	<li class="footText">
+		<h1 class="bur">ACADEMICS</h1>
+		
+			          <ul>
+ 									<li><a  href="#"><i class="fa fa-angle-double-right"></i>Test Series Schedule</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>Postal Coaching</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>PG Dr. Bhatia Books</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>UG Courses</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>Satellite Education</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>Study Centres</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>State P.G. Mocks</a></li>
+                                    <li><a  href="#" target="_blank"><i class="fa fa-angle-double-right"></i>Results</a></li>
+                                    
+                      </ul>
+                    
+	</li>
+                            
+</ul>           
+</div>
+
+<div class="col-lg-4 col-md-4">
+<ul class="footText">
+	<li class="footText">
+		<h1 class="bur">Bratchi</h1>
+		
+			          <ul>
+ 									<li><a  href="#"><i class="fa fa-angle-double-right"></i>  Test Series Schedule</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Postal Coaching</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  PG Dr. Bhatia Books</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  UG Courses</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Satellite Education</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Study Centres</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  State P.G. Mocks</a></li>
+                                    <li><a  href="#" target="_blank"><i class="fa fa-angle-double-right"></i> Results</a></li>
+                                    
+                      </ul>
+                    
+	</li>
+                            
+</ul>           
+</div>
+
+<div class="col-lg-4 col-md-4">
+<ul class="footText">
+	<li class="footText">
+		<h1 class="bur">Bratchi</h1>
+		
+			          <ul>
+ 									<li><a  href="#"><i class="fa fa-angle-double-right"></i>  Test Series Schedule</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Postal Coaching</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  PG Dr. Bhatia Books</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  UG Courses</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Satellite Education</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Study Centres</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  State P.G. Mocks</a></li>
+                                    <li><a  href="#" target="_blank"><i class="fa fa-angle-double-right"></i> Results</a></li>
+                                    
+                      </ul>
+                    
+	</li>
+                            
+</ul>           
+</div>
+	<div class="divider">
+	<hr />
+	</div>
+
+			<div class="col-md-6 col-sm-6 col-lg-6 text-center">
+
+				
+
+					<p class="text">© 2017, Ramon Magsaysay, All rights reserved</p>
+
+			
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 text-center">
+
+				
+
+				<p class="text">Thesis | Web Design & Development by Group 8</p>
+
+
+			</div>
+
+		
+
+		</div>
+	</div>
+</div>
+
+</body>
+
+</html>
